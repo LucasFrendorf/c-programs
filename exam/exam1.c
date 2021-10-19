99d4d01f670d5643d0952612fa2ad4da4ee4e33a
Lucas Frendorf <lucasfrendorf@gmail.com>
2021-10-19 16:25:21 +0200

    Template for exam1

---
+++ exam/exam1.c
@@ -0,0 +1,15 @@
+/* Ran using: gcc filename.c -ansi -Wall -pedantic && ./a.out && rm a.out */
+/* Ran on: Linux [x86_64] */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Symbolic constants */
+
+/* Prototypes */
+
+int main(void) {
+    
+    
+    return EXIT_SUCCESS;
+}
