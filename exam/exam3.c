6f7e534fb52fa5beb8724045cc80c18e31773d6a
Lucas Frendorf <lucasfrendorf@gmail.com>
2021-12-07 00:13:31 +0100

    Team array automaticly detects team and size

--- exam/exam3.c
+++ exam/exam3.c
@@ -1,6 +1,6 @@
 /* Name: Lucas Mørk Frendorf */
 /* Group: B201 */
-/* Ran using: gcc filename.c -ansi -Wall -pedantic && ./a.out && rm a.out */
+/* Ran using: gcc exam3.c -ansi -Wall -pedantic && ./a.out && rm a.out */
 /* Ran on: Linux [x86_64] */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,7 +10,8 @@
 #define WON_POINTS 3
 #define DRAW_POINTS 1
 
-#define TEAMS 12 
+/* Array size initalizer for team array should be > 1 */
+#define ARRAYSTARTSIZE 2 
 
 typedef struct
 {
@@ -36,76 +37,122 @@ typedef struct
 } game;
 
 /* Prototypes */
-int teamNumber(char name[]);
 int compareMatch(const void *a, const void *b);
 
 int main(void) {
     game *Games;
-    team Teams[TEAMS] = {0};
-    int i, lines = 0;
-    int ch;
+    team *Teams;
+    int i, j, arrSize = ARRAYSTARTSIZE, lines = 0, count = 0, add = 0;
+    char team1[4], team2[4];
 
     FILE *fp;
 
     if ((fp = fopen("kampe-2020-2021.txt","r")) == NULL){
         printf("Error! opening file");
+
         return EXIT_FAILURE;
     }
 
-    /* TODO maybe use fseek and double line jump each time */
-    /* Get the amount of games in our files */
-    while(!feof(fp))
+    /* Get the amount of games in our files and get teams */
+    while(fscanf(fp, "%*s %*s %*s %s - %s %*i - %*i %*i", team1, team2) != EOF)
     {
-        ch = fgetc(fp);
+        if (count == 0) {
+            Teams = (team *) calloc(arrSize, sizeof(team));
 
-        if(ch == '\n')
-        {
-            lines++;
+            strcpy(Teams[count].teamName, team1);
+            count++;
+        } else {
+            for (i = 0; i < count; i++)
+            {
+                if (strcmp(Teams[i].teamName, team1) == 0) {
+                    add = 0;
+                    break;
+                }
+
+                add = 1;
+            }
+
+            if (add == 1) {
+                /* Allocating to the power of 2 for more efficiency so we dont have to realloc each time our size increases */
+                if (count >= arrSize) {
+                    arrSize *= arrSize;
+
+                    Teams = realloc(Teams, arrSize * sizeof(team));
+                }
+
+                strcpy(Teams[count].teamName, team1);
+                count++;
+
+                add = 0;
+            }
         }
+        
+        lines++;
     }
 
+    /* Realloc teams to the correct size */
+    Teams = (team *) realloc(Teams, sizeof(team) * (count-1));
     Games = (game *) malloc(sizeof(game) * lines);
 
-    /* Go back to the first line again */
+    /* Go back to the first line again to load in the games array */
     fseek(fp, 0, SEEK_SET );
 
     for (i = 0; i < lines; i++)
     {
         fscanf(fp, "%s %s %s %s - %s %i - %i %i", Games[i].day, Games[i].date, Games[i].clock, Games[i].team1Name, Games[i].team2Name, &Games[i].team1Score, &Games[i].team2Score, &Games[i].spectators);
 
-        strcpy(Teams[teamNumber(Games[i].team1Name)].teamName, Games[i].team1Name);
-        strcpy(Teams[teamNumber(Games[i].team2Name)].teamName, Games[i].team2Name);
+        /* printf("%d\n", count); */
+        for (j = 0; j < count; j++)
+        {
+            /* Realloc doesn't initalize to 0 so we have to do it here */
+            if (i == 0) {
+                Teams[j].points = 0;
+                Teams[j].goalsByTeam = 0;
+                Teams[j].goalsOnTeam = 0;
+            }
 
-        Teams[teamNumber(Games[i].team1Name)].goalsByTeam += Games[i].team1Score;
-        Teams[teamNumber(Games[i].team1Name)].goalsOnTeam += Games[i].team2Score;
+            if (strcmp(Teams[j].teamName, Games[i].team1Name) == 0 || strcmp(Teams[j].teamName, Games[i].team2Name) == 0) {
+                /* Team 1 */
+                if (strcmp(Teams[j].teamName, Games[i].team1Name) == 0) {
+                    Teams[j].goalsByTeam += Games[i].team1Score;
+                    Teams[j].goalsOnTeam += Games[i].team2Score;
 
-        Teams[teamNumber(Games[i].team2Name)].goalsByTeam += Games[i].team2Score;
-        Teams[teamNumber(Games[i].team2Name)].goalsOnTeam += Games[i].team1Score;
+                    if (Games[i].team1Score > Games[i].team2Score)
+                        Teams[j].points += WON_POINTS;
+                    
+                    if (Games[i].team1Score == Games[i].team2Score)
+                        Teams[j].points += DRAW_POINTS;
+                }
 
-        if (Games[i].team1Score == Games[i].team2Score) { /* Equal score */
-            Teams[teamNumber(Games[i].team1Name)].points += DRAW_POINTS;
-            Teams[teamNumber(Games[i].team2Name)].points += DRAW_POINTS;
-        } else if (Games[i].team1Score > Games[i].team2Score) { /* Team 1 won */
-            Teams[teamNumber(Games[i].team1Name)].points += WON_POINTS;
-        } else if (Games[i].team1Score < Games[i].team2Score) { /* Team 2 won */
-            Teams[teamNumber(Games[i].team2Name)].points += WON_POINTS;
+                /* Team 2 */
+                if (strcmp(Teams[j].teamName, Games[i].team2Name) == 0) {
+                    Teams[j].goalsByTeam += Games[i].team2Score;
+                    Teams[j].goalsOnTeam += Games[i].team1Score;
+
+                    if (Games[i].team1Score < Games[i].team2Score)
+                        Teams[j].points += WON_POINTS;
+                    
+                    if (Games[i].team1Score == Games[i].team2Score)
+                        Teams[j].points += DRAW_POINTS;
+                }
+            }
         }
     }
 
     printf("---- Unsorted ----\n");
     printf(" Team | Score | Goals | Goal on team\n");
-    for (i = 0; i < TEAMS; i++)
+    for (i = 0; i < count; i++)
     {
         printf("%5s | %5d | %5d | %5d\n", Teams[i].teamName, Teams[i].points, Teams[i].goalsByTeam, Teams[i].goalsOnTeam);
     }
 
     printf("\n");
 
-    qsort(Teams, TEAMS, sizeof(team), compareMatch);
+    qsort(Teams, count, sizeof(team), compareMatch);
     
     printf("---- Sorted ----\n");
     printf(" Team | Score | Goals | Goal on team\n");
-    for (i = 0; i < TEAMS; i++)
+    for (i = 0; i < count; i++)
     {
         printf("%5s | %5d | %5d | %5d\n", Teams[i].teamName, Teams[i].points, Teams[i].goalsByTeam, Teams[i].goalsOnTeam);
     }
@@ -113,37 +160,6 @@ int main(void) {
     fclose(fp); 
         
     return EXIT_SUCCESS;
-}
-
-int teamNumber(char name[]) {
-    if (strcmp(name ,"SDR") == 0) {
-        return 0;
-    } else if (strcmp(name ,"FCM") == 0) {
-        return 1;
-    } else if (strcmp(name ,"ACH") == 0) {
-        return 2;
-    } else if (strcmp(name ,"RFC") == 0) {
-        return 3;
-    } else if (strcmp(name ,"LBK") == 0) {
-        return 4;
-    } else if (strcmp(name ,"AaB") == 0) {
-        return 5;
-    } else if (strcmp(name ,"BIF") == 0) {
-        return 6;
-    } else if (strcmp(name ,"FCN") == 0) {
-        return 7;
-    } else if (strcmp(name ,"OB") == 0) {
-        return 8;        
-    } else if (strcmp(name ,"FCK") == 0) {
-        return 9;
-    } else if (strcmp(name ,"AGF") == 0) {
-        return 10;
-    } else if (strcmp(name ,"VB") == 0) {
-        return 11;
-    }
-
-    printf("Hey something went wrong!\n");
-    return EXIT_FAILURE;
 }
 
 int compareMatch(const void *a, const void *b) {
